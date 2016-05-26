91be1e8797a2d49c17ae76cb313de8f69cb4a611
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-05-26 23:32:18 +0300

    Localhost is set

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var express = require('express');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/'));
+
+app.set('views', __dirname + '/');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+module.exports = app;
