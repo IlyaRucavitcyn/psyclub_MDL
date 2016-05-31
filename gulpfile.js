ef08edf41586e2e8ba04fcc51272511dd9fd2775
Ilya Rucavitcyn <ilya.rucavitcyn@gmail.com>
2016-05-31 12:34:42 +0300

    Gulp-htmlhint is passed

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp'),
-    htmlHint = require('gulp-htmlhint');
+    htmlHint = require('gulp-htmlhint'),
     // please check https://www.npmjs.com/package/gulp-htmlhint for docs
+    csslint = require('gulp-csslint');
+    // please check https://www.npmjs.com/package/gulp-csslint for docs
 
-gulp.task('html', function() {
+gulp.task('htmllint', function() {
     return gulp.src('./index.html')
         .pipe(htmlHint())
         .pipe(htmlHint.reporter());
+});
+
+gulp.task('csslint', function() {
+  gulp.src('./css/*.css')
+    .pipe(csslint({
+      'box-sizing':false,
+      'adjoining-classes':false,
+      'important':false
+    }))
+    .pipe(csslint.reporter());
 });
